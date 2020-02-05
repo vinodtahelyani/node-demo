e57819d9ff8971be551fa4bf5c50ceaf413390e2
Vinod Tahelyani <vinod.tahelyani@appDynamics.com>
2020-02-05 12:00:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var mongoose=require('mongoose');
+
+
+const app=express();
+
+
+require("appdynamics").profile({
+  controllerHostName: 'master-onprem-controller.e2e.appd-test.com',
+  controllerPort: 8090,
+    accountName: 'customer1',
+  accountAccessKey: '2d6c32c1-1938-4e43-bd7a-7e3c7f31e3bb',
+  applicationName: 'GettingStarted',
+  tierName: 'Tier3',
+  nodeName: 'process' // The controller will automatically append the node name with a unique number
+ });
+ 
+
+app.get('/', (req, res)=>{
+    res.send('Hello world');
+});
+
+
+app.listen(1800);
